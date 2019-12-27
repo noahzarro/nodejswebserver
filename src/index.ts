1dd06ec252262cdc6720f3dfcb975d14e754e84b
zarron <zarron@student.ethz.ch>
2019-12-27 11:02:14 +0100

    initail commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+console.log("starting server");
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "<h1>Ur an fagit</h1>" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
