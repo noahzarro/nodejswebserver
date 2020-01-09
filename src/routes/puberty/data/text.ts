11bf6980efde343958fa40bda09367644b50ed0b
zarron <zarron@student.ethz.ch>
2020-01-09 20:41:56 +0100

    new texts

--- src/routes/puberty/data/text.ts
+++ src/routes/puberty/data/text.ts
@@ -1,72 +1,104 @@
-export const text = [
-  {
-    title: "Loch",
-    msg: "Chunsch au mol wieder zu dim Loch us?",
-    role: "mueter"
-  },
-  {
-    title: "Profitiere",
-    msg: "Immer goots dir nur ums Profitiere",
-    role: "mueter"
-  },
-  {
-    title: "Fresse",
-    msg: "Mampf, Mampf, Mampf",
-    role: "sohn"
-  },
-  {
-    title: "Rueh",
-    msg: "Loh mi doch mol in Rueh",
-    role: "sohn"
-  },
-  {
-    title: "Game",
-    msg: "Muesch du eich immer game?",
-    role: "mueter"
-  },
-  {
-    title: "Chiste",
-    msg: "De Ganz tag hocksch vor dere schiis chiste",
-    role: "mueter"
-  },
-  {
-    title: "Grät",
-    msg: "Leg emol das Grätli weg",
-    role: "mueter"
-  },
-  {
-    title: "Heilandnomol",
-    msg: "Heilandnomol",
-    role: "mueter"
-  },
-  {
-    title: "Gott",
-    msg: "Mein Gott Mama!",
-    role: "sohn"
-  },
-  {
-    title: "Luise",
-    msg: "Ez wörfis denn gad zum Luise übere",
-    role: "mueter"
-  },
-  {
-    title: "Lerne",
-    msg: "Hesch ez scho öppis für d Schuel gmacht?",
-    role: "mueter"
-  },
-  {
-    title: "Kickboxe",
-    msg: "Gosch ez morn is Kickboxe?",
-    role: "mueter"
-  },
-  {
-    title: "Nei",
-    msg: "Nei",
-    role: "sohn"
-  },
-  {
-    title: "Obig",
-    msg: "I lern eh immer erst am obig vorher",
-    role: "sohn"
-  }
-];
+export const text = {
+  data: [
+    {
+      title: "Loch",
+      msg: "Chunsch au mol wieder zu dim Loch us?",
+      role: "mueter"
+    },
+    {
+      title: "Profitiere",
+      msg: "Immer goots dir nur ums Profitiere",
+      role: "mueter"
+    },
+    {
+      title: "Fresse",
+      msg: "Mampf, Mampf, Mampf",
+      role: "sohn"
+    },
+    {
+      title: "Rueh",
+      msg: "Loh mi doch mol in Rueh",
+      role: "sohn"
+    },
+    {
+      title: "Game",
+      msg: "Muesch du eich immer game?",
+      role: "mueter"
+    },
+    {
+      title: "Chiste",
+      msg: "De Ganz tag hocksch vor dere schiis chiste",
+      role: "mueter"
+    },
+    {
+      title: "Grät",
+      msg: "Leg emol das Grätli weg",
+      role: "mueter"
+    },
+    {
+      title: "Heilandnomol",
+      msg: "Heilandnomol",
+      role: "mueter"
+    },
+    {
+      title: "Gott",
+      msg: "Mein Gott Mama!",
+      role: "sohn"
+    },
+    {
+      title: "Luis",
+      msg: "Ez wörfis denn gad zum Luis übere",
+      role: "mueter"
+    },
+    {
+      title: "Lerne",
+      msg: "Hesch ez scho öppis für d Schuel gmacht?",
+      role: "mueter"
+    },
+    {
+      title: "Kickboxe",
+      msg: "Gosch ez morn is Kickboxe?",
+      role: "mueter"
+    },
+    {
+      title: "Prüefug",
+      msg: "Hesch scho glenrt für d Prüefig?",
+      role: "mueter"
+    },
+    {
+      title: "Giet",
+      msg: "Hörsch ez emol uf in Giet inefrässe?",
+      role: "mueter"
+    },
+    {
+      title: "Jo",
+      msg: "Nöd nur jo sege, au jo mache",
+      role: "mueter"
+    },
+    {
+      title: "Hei",
+      msg: "Wenn chunsch hei?",
+      role: "mueter"
+    },
+    {
+      title: "Halbi",
+      msg: "Am halbi",
+      role: "sohn"
+    },
+    {
+      title: "Alt",
+      msg: "De bar alt",
+      role: "mueter"
+    },
+    {
+      title: "Nei",
+      msg: "Nei",
+      role: "sohn"
+    },
+    {
+      title: "Obig",
+      msg: "I lern eh immer erst am obig vorher",
+      role: "sohn"
+    }
+  ]
+};
