06cdea17b6949009edf63d037df4cce6f9168163
zarron <zarron@student.ethz.ch>
2020-01-14 20:18:02 +0100

    added https

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # nodejswebserver
+
+## nodejs / express
+https://developer.okta.com/blog/2018/11/15/node-express-typescript  
+without Materialize and EJS  
+until and without `A Better Way to Manage Configuration Settings in Node.js`
+
+## HTTPS
+https://www.sitepoint.com/how-to-use-ssltls-with-node-js/
+
+## autostart
+```bash
+#!/bin/bash
+
+# lulu
+cd /home/pi/lulu
+nohup python3 lulu_bot.py &
+
+# attila
+cd /home/pi/attila_bot/Python
+nohup python3 Test.py &
+
+# start webserver
+export PATH="$PATH:/usr/local/bin"
+/usr/local/bin/forever start -c /usr/local/bin/node /home/pi/nodejswebserver/dist/index.js
+
+# port forwarding
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 8443
+```
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import cors from "cors";
 import express from "express";
+import fs from "fs";
+import https from "https";
 import path from "path";
 
-import {router as router_decidobot} from "./routes/decidobot/api/decidobot";
-import {router as router_puberty} from "./routes/puberty/api/puberty";
+import { router as router_decidobot } from "./routes/decidobot/api/decidobot";
+import { router as router_puberty } from "./routes/puberty/api/puberty";
 
 const app = express();
 const port = 8080; // default port to listen
+const httpsPort = 8443; // https port
 
 // allow cors
 app.use(cors());
@@ -17,7 +20,34 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/decidobot/api", router_decidobot);
 app.use("/puberty/api", router_puberty);
 
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+// start the http webserver
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+let key;
+let cert;
+
+try {
+  key = fs.readFileSync("server.key");
+  cert = fs.readFileSync("server.cert");
+} catch {
+  key = fs.readFileSync("/etc/letsencrypt/live/www.eth-lerngruppe.ch/fullchain.pem");
+  cert = fs.readFileSync("/etc/letsencrypt/live/www.eth-lerngruppe.ch/privkey.pem");
+}
+
+// start https webserver
+https
+  .createServer(
+    {
+      key,
+      cert,
+    },
+    app
+  )
+  .listen(httpsPort, () => {
+    console.log(
+      `server started at https://localhost:${httpsPort}`
+    );
+  });
