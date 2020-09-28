70c012bb7b271a727c5e40935409acd34cf992fc
zarron <zarron@student.ethz.ch>
2020-09-28 10:45:16 +0200

    added favicon and refactored nav

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import cors from "cors";
+import ejs from "ejs";
 import express from "express";
 import fs from "fs";
 import https from "https";
 import path from "path";
+import favicon from "serve-favicon";
 
 import { router as router_attila_bot } from "./routes/attila-bot/api/attila-bot";
 import { router as router_chat } from "./routes/chat/api/chat";
@@ -20,6 +22,18 @@ app.use(cors());
 // add template engine
 app.set("view engine", "ejs");
 
+// add favicon
+app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
+
+// redirects
+app.get("/", (req, res) => {
+    res.redirect("home");
+});
+
+app.get("/excursions/verkehrshaus", (req, res) => {
+    res.redirect("https://n.ethz.ch/~jgygax/verkehrshaus/");
+});
+
 // define a route handler for the default home page
 app.use(express.static(path.join(__dirname, "..", "public")));
 
@@ -31,6 +45,28 @@ app.use("/puberty/api", router_puberty);
 app.use("/pio-o-mat/api", router_pio_o_mat);
 app.use("/chat/api", router_chat);
 app.use("/attila-bot/api", router_attila_bot);
+
+// load page structure
+const pending = fs.readFileSync("views/pages.json", "utf8");
+const page_structure = JSON.parse(pending);
+
+// render all first level pages
+app.get("/:page", (req, res) => {
+    res.render(req.params.page, {
+        current: req.params.page,
+        page_structure,
+        parent_page: null,
+    });
+});
+
+// render all second level pages
+app.get("/:page/:subpage", (req, res) => {
+    res.render(req.params.page + "/" + req.params.subpage, {
+        current: req.params.page,
+        page_structure,
+        parent_page: req.params.page,
+    });
+});
 
 // start the http webserver
 
